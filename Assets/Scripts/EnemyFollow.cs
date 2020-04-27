0cb5bf3f5a1187c47df93476a22f4c689999b4ef
Andy <andyocampo@users.noreply.github.com>
2020-04-27 18:00:29 -0500

    Added first objects

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+
+    void Start()
+    {
+        transform.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.Translate(transform.position - Player.transform.position);
+    }
+}
